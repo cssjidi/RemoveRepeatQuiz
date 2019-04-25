9b5b93415058532b68730b4ba43b15494cc74a47
css9 <cjd2015@qq.com>
2019-04-25 09:04:25 +0800

    添加项目文件。

---
+++ ClearRepeate/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ClearRepeate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "上传（word不能大于2M）";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(14, 63);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(667, 367);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(216, 17);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "保存文档";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "帮助";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(437, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 36);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "免责声明";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "文件上传中，请稍候...";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(693, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "华师题库去重程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ClearRepeate/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using NPOI.XWPF.UserModel;
+
+namespace ClearRepeate
+{
+    public partial class Form1 : Form
+    {
+        private static List<string> list;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = this.openFileDialog1;
+            dlg.Filter = "word文件|*.docx";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                button1.Enabled = false;
+                label1.Visible = true;
+                button2.Enabled = false;
+                button4.Enabled = false;
+                string fileName = dlg.FileName;
+                FileInfo info = new FileInfo(fileName);
+                if (info.Length / 1024 > 2048)
+                {
+                    MessageBox.Show("上传文件不能超过2M");
+                    return;
+                }
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                XWPFDocument myDocx = new XWPFDocument(fs);//打开07（.docx）以上的版本的文档
+
+                list = new List<string>();
+                string compareStr = "";
+                bool similar = false;
+                int index = 0;
+                foreach (var para in myDocx.Paragraphs)
+                {
+                    
+                    string strSence = para.ParagraphText;
+                    if (strSence.Contains("我的答案") || strSence.Contains("得分"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(strSence, @"^(\d)+、?"))
+                        {
+                            compareStr = Regex.Replace(strSence, @"^(\d)+、?", "");
+                            similar = false;
+                            if (list.Exists(n => n.Contains(compareStr)))
+                            {
+                                similar = true;
+                                compareStr = "";
+                            }
+                            else
+                            {
+                                index++;
+                                list.Add(index.ToString() + "、" +compareStr);
+                            }
+                        }
+                        else if (similar)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            list.Add(strSence);
+                        }
+                    }
+                }
+                fs.Close();
+                fs.Dispose();
+
+                button3.Enabled = true;
+                label1.Visible = false;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                textBox1.Enabled = true;
+                viewInTextBox();
+            }
+        }
+
+        private void viewInTextBox() {
+            string str = "";
+            list.ForEach(n => {
+                str += n + "\r\n";
+            });
+            textBox1.ScrollToCaret();
+            textBox1.Text = str;
+        }
+
+        private void SaveFileAsWrod() {
+            var doc = new XWPFDocument();
+            list.ForEach(n => {
+                var p1 = doc.CreateParagraph();
+                p1.Alignment = ParagraphAlignment.LEFT;
+                var runTitle = p1.CreateRun();
+                if (Regex.IsMatch(n, @"^(\d)+、?"))
+                {
+                    runTitle.FontSize = 14;
+                    runTitle.SetFontFamily("微软雅黑", FontCharRange.None);
+                    runTitle.SetText(n.Trim() + "\r\n");
+                } else if (n.Contains("参考答案"))
+                {
+                    runTitle.SetColor("#f00");
+                    runTitle.FontSize = 14;
+                    runTitle.SetText(n + "\r\n\r\n");
+                }
+                else if (!n.Equals("\r\n")) {
+                     runTitle.SetText(n + "\r\n");
+                }
+            });
+            //runTitle.FontSize = 12;
+            //runTitle.SetFontFamily("微软雅黑", FontCharRange.None);
+            //var ms = new MemoryStream();
+            //doc.Write(ms);
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = DateTimeKind.Local.ToString();
+            sfd.Filter = "Word Document(*.docx)|*.docx";
+            sfd.DefaultExt = "Word Document(*.docx)|*.docx";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                button1.Enabled = false;
+                button3.Enabled = false;
+                FileStream fs = (FileStream)sfd.OpenFile();
+                doc.Write(fs);
+                fs.Close();
+                doc.Close();
+                doc = null;
+                fs = null;
+            }
+            MessageBox.Show("保存成功");
+            button1.Enabled = true;
+            button3.Enabled = true;
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileAsWrod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            printDialog1.ShowDialog();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.BackColor = Color.Transparent;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("点击上传需要处理的word文档，点击保存保存去重后的文档");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("本软件只授权给华师网络学院学生使用，如商用或除华师网络学院学生外的使用的，请联系cjd2015@qq.com进行授权，特此声明");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
